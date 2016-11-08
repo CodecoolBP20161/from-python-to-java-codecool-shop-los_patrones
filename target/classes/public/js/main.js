9b3b190de2a1940056d36d50c157f5c1437b279c
handriss <andrashinkel@gmail.com>
2016-11-08 17:02:32 +0100

    + and - buttons starting to work

--- target/classes/public/js/main.js
+++ target/classes/public/js/main.js
@@ -2,17 +2,33 @@ $('document').ready(function(){
 
     $('#cartModalButton').click(function(){
         $.ajax({url: "/cart", success: function(result){
-            result = JSON.parse(result);
-            console.log(result);
 
-            var table_body = document.getElementById("cicamica")
+            result = JSON.parse(result);
+            var table_body = document.getElementById("modal-tbody")
+            table_body.innerHTML = "";
 
             for(var i = 0; i < result.prices.length; i++){
 
                 var row = document.createElement("tr");
-                row.innerHTML = "<td>" + result.names[i] + "</td><td>" + result.prices[i]+"</td><td>" + result.quantites[i] + "</td>"
+                row.id = "valami";
+                row.innerHTML = "<td>" + result.names[i] + "</td>" +
+                                "<td>" + result.prices[i]+"</td>" +
+                                "<td>" + result.quantites[i] + "</td>";
+                                // "<td><span class='glyphicon glyphicon-plus'></span></td>" +
+                                // "<td><span class='glyphicon glyphicon-minus'></span></td>";
                 table_body.appendChild(row);
-                
+
+                var $button = $('<button/>', {
+                    type: 'button',
+                    'class': 'glyphicon glyphicon-plus',
+                    id: result.names[i],
+                    text: 'Here!',
+                    click: function() {
+                        window.alert('Hello! My id is '+ this.id);
+                    }
+                });
+
+                $button.appendTo('#valami');
             }
         }});
     });
