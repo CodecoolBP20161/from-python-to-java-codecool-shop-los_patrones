e6fe7bd597e36708d13becacd635a59016410b57
tbalint19 <toth910719balint@gmail.com>
2016-11-10 18:44:29 +0100

    Sessionlogger class skeleton created

---
+++ src/main/java/services/SessionLogger.java
@@ -0,0 +1,21 @@
+package services;
+
+
+public class SessionLogger {
+
+    public void logPutIntoCartEvent(String id) {
+
+    }
+
+    public void logDeleteFromCartEvent(String id) {
+
+    }
+
+    public void logCheckoutEvent(String id) {
+
+    }
+
+    public void logPayEvent(String id) {
+
+    }
+}
