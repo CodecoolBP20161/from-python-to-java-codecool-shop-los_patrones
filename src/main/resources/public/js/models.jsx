bf05e2aefe22b0689aa8d8f1033adf4ed0fd2462
handriss <andrashinkel@gmail.com>
2016-11-22 19:35:46 +0100

    all rendering works, eventlisteners remain to be done

---
+++ src/main/resources/public/js/models.jsx
@@ -0,0 +1,62 @@
+var x = 10;
+
+var SupplierBarComponent = React.createClass({
+    doSomething: function () {
+        console.log("aaaaaaaah");
+        x = x+5;
+    },
+
+    render: function() {
+        const suppliers = this.props.data.map(function(supplier){
+            return (
+                <option id={'supplier' + supplier.id} onClick={this.doSomething} key={supplier.id}>{supplier.name}</option>
+            );
+        }, this);
+
+        return(
+            <div>{suppliers}</div>
+        );
+
+    }
+});
+
+var CategoryBarComponent = React.createClass({
+    render: function() {
+
+        const categories = this.props.data.map(function(category){
+            return (
+                <option key={category.id}>{category.name}</option>
+            );
+        });
+
+        return(
+            <div>{categories}</div>
+        );
+
+    }
+});
+
+var ProductComponent = React.createClass({
+    render: function() {
+
+        const currentProducts = this.props.data.map(function(product){
+            return(
+                <div key={product.id} className="item col-xs-4 col-sm-4 col-md-4 col-lg-4 thumbnail">
+                    <img className="group list-group-image" src={"/img/product_" + product.id + ".jpg"}/>
+                    <div className="caption">
+                        <h4 className="group inner list-group-item-heading">{product.name}</h4>
+                        <p className="group inner list-group-item-text">{product.description}</p>
+                        <p>
+                            <span>{product.price} {product.currency}</span>
+                            <button id={"getProduct/" + product.id} className="btn btn-success">Add to cart</button>
+                        </p>
+                    </div>
+                </div>
+            );
+        });
+
+        return (
+            <div>{currentProducts}</div>
+        );
+    }
+});
--- target/classes/public/js/main.jsx
+++ target/classes/public/js/main.jsx
@@ -3,7 +3,7 @@ function Model(cart, categories, suppliers, products){
     this.cart = cart;
     this.categories = categories;
     this.suppliers = suppliers;
-    this.products = products['Products'];
+    this.products = products;
 }
 
 var controller = {
@@ -11,75 +11,39 @@ var controller = {
         var getCartData = $.ajax({ url: '/cart' }),
             getCategoryData = $.ajax({ url: '/categories' }),
             getSupplierData = $.ajax({ url: '/suppliers'}),
-            getProdcuts = $.ajax({url: '/products'});
+            getProducts = $.ajax({url: '/products'});
 
-        // Main controller logic starts when all data are loaded in
-        $.when(getCartData, getCategoryData, getSupplierData, getProdcuts
+        $.when(getCartData, getCategoryData, getSupplierData, getProducts
         ).done( function( cart, categories, suppliers, products ) {
-            var model = new Model(cart, categories, JSON.parse(suppliers[0]), JSON.parse(products[0]));
-            React.render(<ProductComponent data={model}/>, document.getElementById('products'));
+
+            var model = new Model(cart, JSON.parse(categories[0]), JSON.parse(suppliers[0]), JSON.parse(products[0]));
+            React.render(<ProductComponent data={model.products}/>, document.getElementById('products'));
             React.render(<SupplierBarComponent data={model.suppliers}/>, document.getElementById('searchSupplier'));
             React.render(<CategoryBarComponent data={model.categories}/>, document.getElementById('searchCategory'));
 
 
         });
+    },
+    selectSupplier: function(){
+        console.log("selectSupplier");
     }
 };
 
-var SupplierBarComponent = React.createClass({
-    render: function() {
-        const supplierData = this.props.data;
-        console.log(supplierData);
-        console.log(typeof supplierData);
-
-
-        const suppliers = supplierData.map(function(supplier){
-            return (
-                <option>{supplier.name}</option>
-            );
-        });
-
-        return(
-            <div>{suppliers}</div>
-        );
-    }
+document.body.addEventListener("click", function(event) {
+    console.log("sfwegfweg");
+    console.log(event.target.id);
 });
 
-var CategoryBarComponent = React.createClass({
-    render: function() {
-        return(
-            <p>mica</p>
-        );
+// $(document.body).on('click', '#supplier1', function() {
+//     //do something
+//     console.log("supplier1 clicked");
+// });
+//
+// $(document.body).on('click', '#supplier2', function() {
+//     //do something
+//     console.log("supplier2 clicked");
+// });
 
-    }
-});
-
-var ProductComponent = React.createClass({
-    render: function() {
-
-        const products = this.props.data.products;
-        console.log(products);
-        const currentProducts = products.map(function(product){
-            return(
-                <div className="item col-xs-4 col-sm-4 col-md-4 col-lg-4 thumbnail">
-                    <img className="group list-group-image" src={"/img/product_" + product.id + ".jpg"}/>
-                    <div className="caption">
-                        <h4 className="group inner list-group-item-heading">{product.name}</h4>
-                        <p className="group inner list-group-item-text">{product.description}</p>
-                        <p>
-                            <span>{product.price} {product.currency}</span>
-                            <button id={"getProduct/" + product.id} className="btn btn-success">Add to cart</button>
-                        </p>
-                    </div>
-                </div>
-            );
-        });
-
-        return (
-            <div>{currentProducts}</div>
-        );
-    }
-});
 
 
 controller.initApp();
