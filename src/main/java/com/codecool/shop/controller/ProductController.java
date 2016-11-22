651d6db5696575d5fb992202ad52d133e3790fb1
turbek <turbek93@gmail.com>
2016-11-22 09:37:39 +0100

    getProducts() refactored

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -7,19 +7,19 @@ import com.codecool.shop.dao.implementation.LineItem;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
-import com.codecool.shop.model.*;
+import com.codecool.shop.model.BaseModel;
+import com.codecool.shop.model.Cart;
+import com.codecool.shop.model.Product;
 import com.codecool.shop.services.CartService;
 import com.codecool.shop.services.JsonTransformer;
 import com.codecool.shop.services.SessionLogger;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.stream.Collectors;
 
 
@@ -74,52 +74,28 @@ public class ProductController {
     }
 
     public static String getProducts (Request req, Response res) {
-        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
-        Map allParams = new HashMap();
-        if (req.queryParams("category") != null) {
-            allParams.put("category", req.queryParams("category"));
-        }
-        if (req.queryParams("supplier") != null) {
-            allParams.put("supplier", req.queryParams("supplier"));
-        }
+        JsonTransformer transformer = new JsonTransformer();
+        ProductDao pDao = ProductDaoMem.getInstance();
+        ProductCategoryDao cDao = ProductCategoryDaoMem.getInstance();
+        SupplierDao sDao = SupplierDaoMem.getInstance();
+        String category = req.queryParams("category");
+        String supplier = req.queryParams("supplier");
+        List<Product> allProducts = null;
 
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-
-        Map responseData = new HashMap<String, ArrayList>();
-        ArrayList productList = new ArrayList<String>();
-
-        ArrayList possibleCategories = new ArrayList<ProductCategory>();
-        if (allParams.containsKey("category")) {
-            String index = (String) allParams.get("category");
-            possibleCategories.add(productCategoryDataStore.find((Integer.parseInt(index))));
+        if (category != null && supplier != null) {
+            allProducts = pDao.getBy(cDao.find(Integer.parseInt(category)), sDao.find(Integer.parseInt(supplier)));
         }
-        else {
-            for (int i=0; i<ProductCategoryDaoMem.getDATA().size(); i++) {
-                possibleCategories.add(ProductCategoryDaoMem.getDATA().get(i));
-            }
+        if (category != null && supplier == null) {
+            allProducts = pDao.getBy(cDao.find(Integer.parseInt(category)));
+        }
+        if (category == null && supplier != null) {
+            allProducts = pDao.getBy(sDao.find(Integer.parseInt(supplier)));
+        }
+        if (category == null && supplier == null) {
+            allProducts = pDao.getAll();
         }
 
-        ArrayList possibleSuppliers = new ArrayList<Supplier>();
-        if (allParams.containsKey("supplier")) {
-            String index = (String) allParams.get("supplier");
-            possibleSuppliers.add(supplierDataStore.find((Integer.parseInt(index))));
-        }
-        else {
-            for (int i=0; i<SupplierDaoMem.getDATA().size(); i++) {
-                possibleSuppliers.add(SupplierDaoMem.getDATA().get(i));
-            }
-        }
-
-        for (int i=0; i<productDataStore.getAll().size(); i++) {
-            if (possibleSuppliers.contains(productDataStore.getAll().get(i).getSupplier()) && possibleCategories.contains(productDataStore.getAll().get(i).getProductCategory())) {
-                productList.add(productDataStore.getAll().get(i).toDict());
-            }
-        }
-
-        responseData.put("Products", productList);
-
-        return gson.toJson(responseData);
+        ArrayList returnData = allProducts.stream().map(Product::toDict).collect(Collectors.toCollection(ArrayList::new));
+        return transformer.render(returnData);
     }
 }
