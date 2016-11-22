e7f230e81f79e0336cbce7a0733fe11bc7bc7cbe
handriss <andrashinkel@gmail.com>
2016-11-22 12:05:05 +0100

    ProductComponent button id and image source finished

--- src/main/resources/public/js/main.jsx
+++ src/main/resources/public/js/main.jsx
@@ -29,81 +29,25 @@ var ProductComponent = React.createClass({
 
         const products = this.props.data.products;
 
-        // const currentProducts = (
-        //     <div>cica</div>
-        // );
-
         const currentProducts = products.map(function(product){
             return(
                 <div className="item col-xs-4 col-sm-4 col-md-4 col-lg-4 thumbnail">
-                    <img className="group list-group-image" src=""/>
+                    <img className="group list-group-image" src={"/img/product_" + product.id + ".jpg"}/>
                     <div className="caption">
                         <h4 className="group inner list-group-item-heading">{product.name}</h4>
                         <p className="group inner list-group-item-text">{product.description}</p>
                         <p>
                             <span>{product.price} {product.currency}</span>
-                            <button id="d" className="btn btn-success">Add to cart</button>
+                            <button id={"getProduct/" + product.id} className="btn btn-success">Add to cart</button>
                         </p>
                     </div>
                 </div>
             );
         });
 
-
         return (
             <div>{currentProducts}</div>
         );
-
-        // const renderProducts = products => (
-        //     products.map(product => renderProduct(product))
-        // );
-        //
-        // const renderProduct = product => (
-        //     <div className="item col-xs-4 col-sm-4 col-md-4 col-lg-4 thumbnail">
-        //         <img className="group list-group-image" src=""/>
-        //         <div className="caption">
-        //             <h4 className="group inner list-group-item-heading">cica név</h4>
-        //             <p className="group inner list-group-item-text">cica leírás</p>
-        //             <p>
-        //                 <span>cica ár és currency</span>
-        //                 <span>nemtommi</span>
-        //                 <span>{product}</span>
-        //                 <button id="'getProduct/' + {product.id}" className="btn btn-success">Add to cart</button>
-        //             </p>
-        //         </div>
-        //     </div>
-        // );
-
-        // const renderProduct = product => (
-        //     <div className="item col-xs-4 col-sm-4 col-md-4 col-lg-4 thumbnail">
-        //         <img className="group list-group-image" src=""/>
-        //         <div className="caption">
-        //             <h4 className="group inner list-group-item-heading">cica név</h4>
-        //             <p className="group inner list-group-item-text">cica leírás</p>
-        //             <p>
-        //                 <span>cica ár és currency</span>
-        //                 <span>nemtommi</span>
-        //                 <span>{product}</span>
-        //                 <button id="'getProduct/' + {product.id}" className="btn btn-success">Add to cart</button>
-        //             </p>
-        //         </div>
-        //     </div>
-        // );
-        //
-        // const renderProducts = products => (
-        //     products.map(product => renderProduct(product))
-        // );
-        //
-        // const currentProducts = props => {
-        //     const products = renderProducts(props.data.products);
-        //
-        //     return (
-        //         <section>
-        //             {products}
-        //         </section>
-        //     );
-        // };
-        // return currentProducts();
     }
 });
 
