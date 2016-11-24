6dffe90ee519f04e40de8dacfd4340b5d347e02c
turbek <turbek93@gmail.com>
2016-11-24 15:27:37 +0100

    CartDaoTest written for CartDaoMem

--- src/test/java/com/codecool/shop/dao/CartDaoTest.java
+++ src/test/java/com/codecool/shop/dao/CartDaoTest.java
@@ -1,8 +1,13 @@
 package com.codecool.shop.dao;
 
+import com.codecool.shop.dao.implementation.daomem.CartDaoMem;
 import com.codecool.shop.model.Cart;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class CartDaoTest {
 
@@ -11,12 +16,31 @@ public class CartDaoTest {
 
     @Before
     public void setUp() throws Exception {
+        cart = new Cart();
 
+        dao = CartDaoMem.getInstance();
+    }
+
+    @Test
+    public void testAddandFind() throws Exception {
+        dao.add(cart);
+        assertEquals(cart, dao.find(1));
+    }
+
+    @Test
+    public void testFindNonexistent() throws Exception {
+        assertEquals(null, dao.find(-1));
+    }
+
+    @Test
+    public void testSingleton() throws Exception {
+        assertEquals(dao, CartDaoMem.getInstance());
     }
 
     @After
     public void tearDown() throws Exception {
-
+        cart = null;
+        dao = null;
     }
 
 }
