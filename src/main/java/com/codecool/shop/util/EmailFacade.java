800e299f658c8aa5b0f75463fd494e494ba4644a
turbek <turbek93@gmail.com>
2016-12-08 15:50:19 +0100

    EmailFacade implemented

--- src/main/java/com/codecool/shop/util/EmailFacade.java
+++ src/main/java/com/codecool/shop/util/EmailFacade.java
@@ -2,14 +2,50 @@ package com.codecool.shop.util;
 
 import com.codecool.shop.model.User;
 
-/**
- * Created by balint on 12/8/16.
- */
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+
 public class EmailFacade {
 
     public EmailFacade () {}
 
-    public void sendWelcome(User user) {
+    public void sendWelcome(User user) { // User user
 
+        final String username = "testerzh1234@gmail.com";
+        final String password = "chillcoders";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("testerzh1234@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(user.getEmail()));
+            message.setSubject("Welcome to our precious shop!");
+            message.setText("Dear "+ user.getName() +","
+                    + "\n\n NYANYANYANYA! THIS IS A NO SPAM!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
